0b6f77945f9e94f3b770aab281cfcb1096126361
Кирилл Ковалев <k.kovalev.sys@gmail.com>
2019-10-30 06:33:59 +0300

    service-worker

--- next.config.js
+++ next.config.js
@@ -3,6 +3,30 @@ const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
+    // devSwSrc: './public/service-worker.js',
+    // generateSw: false,
+    generateInDevMode: true,
+    swDest: 'service-worker.js',
+    transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+    workboxOpts: {
+        runtimeCaching: [
+            {
+                urlPattern: /^https?.*/,
+                handler: 'NetworkFirst',
+                options: {
+                    cacheName: 'https-calls',
+                    networkTimeoutSeconds: 10,
+                    expiration: {
+                        maxEntries: 150,
+                        maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+                    },
+                    cacheableResponse: {
+                        statuses: [0, 200],
+                    },
+                },
+            },
+        ],
+    },
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
         /**
         Note: we provide webpack above so you should not `require` it
